25a24f9764abbd9f1a3837d6bf8c84022023fa40
Faham <129623271+iamfaham@users.noreply.github.com>
2024-08-01 22:39:11 +0530

    Bug fix

--- components/ExperienceSection.tsx
+++ components/ExperienceSection.tsx
@@ -23,9 +23,9 @@ export default function ExperienceSection() {
   ];
 
   return (
-    <section id="experience" className="section w-full py-12 md:py-24 lg:py-32">
+    <section id="experience" className="section w-full py-8 md:py-24 lg:py-32">
       <div className="container max-w-6xl mx-auto px-4 md:px-6">
-        <div className="flex flex-col items-center text-center space-y-6">
+        <div className="flex flex-col items-center text-center space-y-4">
           <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">My Experience</h2>
           <p className="text-lg md:text-xl leading-relaxed text-muted-foreground">
             Here are some of the companies I&apos;ve worked with.
@@ -33,10 +33,10 @@ export default function ExperienceSection() {
           <div className="grid gap-4 md:gap-8 sm:grid-cols-2 lg:grid-cols-3">
             {experiences.map((experience, index) => (
               <ShineBorder key={index} color={["#87CEEB", "#A020F0", "#00FFFF"]}>
-                <div className=" rounded-lg p-4 md:p-6 shadow-lg">
+                <div className=" rounded-lg p-2 md:p-6 shadow-lg">
                   <h3 className="text-xl md:text-2xl font-semibold">{experience.role}</h3>
-                  <p className="text-md text-muted-foreground italic">{experience.company}</p>
-                  <p className="text-sm text-muted-foreground">{experience.duration}</p>
+                  <p className="text-sm md:text-md text-muted-foreground italic">{experience.company}</p>
+                  <p className="text-xs md:text-sm text-muted-foreground">{experience.duration}</p>
                   <p className="text-md mt-4">{experience.description}</p>
                 </div>
               </ShineBorder>
